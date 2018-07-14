b99d9a8ad77a40875286a9d5c296848e5aebcaff
Alex Sole <40474829+AlexDateDev@users.noreply.github.com>
2018-07-14 20:29:54 +0200

    Php GUID

--- GUID/IsGUID.php
+++ GUID/IsGUID.php
@@ -1,3 +1,18 @@
 <?php
 
-?>
+/**
+ * determines if a passed string matches the criteria for a Sugar GUID
+ * @param string $guid
+ * @return bool False on failure
+ */
+function is_guid($guid) {
+	if(strlen($guid) != 36) {
+		return false;
+	}
+
+	if(preg_match("/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/i", $guid)) {
+		return true;
+	}
+
+	return true;;
+}
