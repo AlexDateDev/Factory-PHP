165766ea89d19ae89da097e12ce17ae88cc0e846
Alex Sole <40474829+AlexDateDev@users.noreply.github.com>
2018-07-14 20:41:51 +0200

    Php Email

--- EMail/PhtMailer/Enviar con Atexsa.php
+++ EMail/PhtMailer/Enviar con Atexsa.php
@@ -1,3 +1,28 @@
 <?php
-
+// ----------------------------------------------------------------------------
+// Envio con Atexsa
+//
+//
+//
+// Date : 21/02/2014
+// By   : Type here your name or nickname.
+// ----------------------------------------------------------------------------
 ?>
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->From = 'asole@atexsa.es';
+$mail->FromName = 'Alex Sole';
+$mail->Subject = 'Asunto';
+$body = 'Cuerpo del mensaje';
+$mail->MsgHTML($body);
+$mail->IsHTML(true);
+/* Atexsa */
+$mail->Host = '192.168.2.3';
+$mail->Port = 25;
+$mail->Username = 'etransnetdesarrollo';
+$mail->Password = 'EDatexsa1';
+$mail->SMTPAuth = true;
+if(!$mail->Send()) {alert( 'Error enviando: ' . $mail->ErrorInfo );} else {    echo 'Enviado';    }}
+
+                      
--- EMail/PhtMailer/Envio con Google.php
+++ EMail/PhtMailer/Envio con Google.php
@@ -1,3 +1,36 @@
 <?php
-
+// ----------------------------------------------------------------------------
+// Envio com Google
+//
+//
+//
+// Date : 21/02/2014
+// By   : Type here your name or nickname.
+// ----------------------------------------------------------------------------
 ?>
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->From = 'asole@atexsa.es';
+$mail->FromName = 'Alex Sole';
+
+$mail->Subject = 'Asunto';
+$body = 'Cuerpo del mensaje';
+$mail->MsgHTML($body);
+$mail->IsHTML(true);
+
+$mail->AddAddress( 'asole@atexsa.es');
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = 'alex.atexsa@gmail.com';
+$mail->Password = 'alex1atexsa';
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+
+if(!$mail->Send()) {             
+    alert( 'Error enviando: ' . $mail->ErrorInfo );
+} else {
+    echo 'Enviado';
+}
+
