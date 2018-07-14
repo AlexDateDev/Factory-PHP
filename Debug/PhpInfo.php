19105f6f61d91b6b73e6d80e3559e8ed35f89976
Alex Sole <40474829+AlexDateDev@users.noreply.github.com>
2018-07-14 23:24:21 +0200

    Php Debug

---
+++ Debug/PhpInfo.php
@@ -0,0 +1,22 @@
+<?php
+// ----------------------------------------------------------------------------
+// PhpInfo_php
+//
+//
+//
+// Date : 16/10/2013
+// By   : Type here your name or nickname.
+// ----------------------------------------------------------------------------
+?>
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
+                              
