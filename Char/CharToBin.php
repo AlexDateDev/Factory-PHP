b62e541237a6a9d3b7c4573fbe07ccb7f8b3230e
Alex Sole <40474829+AlexDateDev@users.noreply.github.com>
2018-07-15 00:09:00 +0200

    Php Char

--- Char/CharToBin.php
+++ Char/CharToBin.php
@@ -10,9 +10,9 @@
 
 // Convertir caracteres o numeros a binario, se trienen que empaquetar
 
-$bin = pack('c',0), 0); // -- Anade "\0" => 0x0 (NULL)
-$bin = pack('c',1), 1); // -- Anade el valor 1 en binario 0x1 ( (No el caracter 1)
-$bin = pack('c',2), 2); // -- Anade el valor 2 en binario 0x2 (STX) (No el caracter 2)
+$bin = pack('c',0, 0); // -- Anade "\0" => 0x0 (NULL)
+$bin = pack('c',1, 1); // -- Anade el valor 1 en binario 0x1 ( (No el caracter 1)
+$bin = pack('c',2, 2); // -- Anade el valor 2 en binario 0x2 (STX) (No el caracter 2)
 
 //Comprobar si un caracter es binrio
 
