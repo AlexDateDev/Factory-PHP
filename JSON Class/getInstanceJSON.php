ced8b81459336e2be0ef31db52fafbb963f1caa7
Alex Sole <40474829+AlexDateDev@users.noreply.github.com>
2018-07-15 18:29:29 +0200

    Php JSON

--- JSON Class/getInstanceJSON.php
+++ JSON Class/getInstanceJSON.php
@@ -1,3 +1,14 @@
 <?php
 
-?>
+/**
+ * Singleton to return JSON object
+ * @return	JSON object
+ */
+function getJSONobj() {
+	static $json = null;
+	if(!isset($json)) {
+		require_once('include/JSON.php');
+		$json = new JSON(JSON_LOOSE_TYPE);
+	}
+	return $json;
+}
